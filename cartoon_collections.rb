6859eb26765d6af483f34c98799b19cf67bba1c8
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-07 10:55:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,10 +30,7 @@ end
 def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-    if array.include?(cheese_types)
       array.detect {|cheese| 
         cheese_types.include?(cheese)}
-    elsif array.none? {|cheese| (cheese == "cheddar" || "gouda" || "camembert")}
-      nil 
     end 
 end
